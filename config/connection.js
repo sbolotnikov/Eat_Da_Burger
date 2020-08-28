9818d1d61e207013e677dd9a612c85c09cba267f
Sergey Bolotnikov <sbolotnikov@gmail.com>
2020-08-28 16:30:41 -0400

    conneting db

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "us-cdbr-east-02.cleardb.com",
   port: 3306,
-  user: "root",
-  password: "asdf1234",
-  database: "burgers_db"
+  user: "b33c0c3b2bbb19",
+  password: "e7bb66c2",
+  database: "heroku_f13818dc98aac07"
 });
 
 // Make connection.
--- server.js
+++ server.js
@@ -23,7 +23,8 @@ var routes = require("./controllers/burgersController.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT,()=> {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
+
