19a2e46e5ac30f39841851b96977a6d292a76805
Sergey Bolotnikov <sbolotnikov@gmail.com>
2020-10-09 06:42:25 -0400

    CORS issue 2

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -6,34 +6,34 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   burger.read()
-  .then( data => {
-    var hbsObject = {
-      burgers: data
-    };
-    res.render("index", hbsObject);
-  })
-  .catch( err => {
-    console.log(err);
-  });
+    .then(data => {
+      var hbsObject = {
+        burgers: data
+      };
+      res.render("index", hbsObject);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
-router.post("/api/burgers", function(req, res) {
+router.post("/api/burgers", function (req, res) {
   var cols = Object.entries(req.body).map(e => e[0]); // get columns
   var vals = Object.entries(req.body).map(e => e[1]); // get values
-  
+
   burger.create(cols, vals)
-  .then( results => {
-    console.log(results);
-    res.json({id: results.insertId});
-  })
-  .catch( err => {
-    console.log(err);
-  });
+    .then(results => {
+      console.log(results);
+      res.json({ id: results.insertId });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
@@ -41,54 +41,53 @@ router.put("/api/burgers/:id", function(req, res) {
   burger.update({
     devoured: req.body.devoured
   }, condition)
-  .then( result => {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  })
-  .catch( err => {
-    console.log(err);
-  });
+    .then(result => {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
-router.delete("/api/burgers/:id", function(req, res) {
+router.delete("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   burger.delete(condition)
-  .then( result => {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  })
-  .catch( err => {
-    console.log(err);
-  });
+    .then(result => {
+      if (result.affectedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 const axios = require("axios");
-router.get("/proxy/api/v1:link", function(req, res) {
-  let url_1=req.params.link.slice(7) + process.env.APIKey;
+router.get("/proxy/api/v1:link", function (req, res) {
+  let url_1 = req.params.link.slice(7) + process.env.APIKey;
 
   axios
-  .get(url_1)
-  .then(function(response) {
-    console.log(response.data.results);
-    // fixing CORS
-    res.header('Access-Control-Allow-Origin', '*');
-    res.send(response.data);
-  });
+    .get(url_1)
+    .then(function (response) {
+      // fixing CORS
+      res.header('Access-Control-Allow-Origin', '*');
+      res.send(response.data);
+    });
 });
-router.get("/proxy/api/key", function(req, res) {
-  
-    // res.header('Access-Control-Allow-Origin', '*');
-    res.send(process.env.APISpoon);
-  
+router.get("/proxy/api/key", function (req, res) {
+  // fixing CORS
+  res.header('Access-Control-Allow-Origin', '*');
+  res.send(process.env.APISpoon);
+
 });
 // Export routes for server.js to use.
 module.exports = router;
